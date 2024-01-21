9a8aa1af21acffb98dbf270bc6f6d1de12745d9b
Nicholas-Battista <battistan23@gmail.com>
2024-01-21 16:09:08 -0700

    created starting functions in js
    object constructor to read in new books, function to add the book to the
    library array, interate through array to display the book

---
+++ script.js
@@ -0,0 +1,39 @@
+const content = document.querySelector(".content");
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addToLibrary(book) {
+  myLibrary.push(book);
+}
+
+const theHobbit = new Book("hobbit", "JRR TOLK", 250, "i have read");
+addToLibrary(theHobbit);
+
+myLibrary.forEach((book) => {
+  let div = document.createElement("div");
+
+  let title = document.createElement("p");
+  title.innerHTML = book.title;
+  div.appendChild(title);
+
+  let author = document.createElement("p");
+  author.innerHTML = book.author;
+  div.appendChild(author);
+
+  let pages = document.createElement("p");
+  pages.innerHTML = book.pages;
+  div.appendChild(pages);
+
+  let read = document.createElement("p");
+  read.innerHTML = book.read;
+  div.appendChild(read);
+
+  div.classList.add("book");
+  content.appendChild(div);
+});
